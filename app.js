2cdbab501d40f8eca3bcc1141c40a7cba4a5b19d
Finalgalaxy <finalgalaxy@gmail.com>
2017-07-03 17:49:50 +0200

    Retrieving channel last 50 uploaded videos.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var request = require("request");
 var config = require("./config");
 
-request("https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername=NoCopyrightSounds&key="+config.youtube_api_key+"&part=contentDetails", function(error, response, body) {
-  console.log(body);
+request("https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=NoCopyrightSounds&key="+config.youtube_api_key, function(error, response, body) {
+  var upload_id = JSON.parse(body).items[0].contentDetails.relatedPlaylists.uploads;
+  console.log("Upload_ID="+upload_id);
+  request("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId="+upload_id+"&maxResults=50&key="+config.youtube_api_key, function(error, response, body) {
+    console.log(body);
+  });
 });
