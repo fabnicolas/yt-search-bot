8e2bd074971cff78ce4d7bca4b4d6481726fbded
Finalgalaxy <finalgalaxy@gmail.com>
2017-07-22 17:49:01 +0200

    Refactored comments.

--- bot.js
+++ bot.js
@@ -11,41 +11,32 @@ bot.telegram.getMe().then((bot_informations) => {
     console.log("Server has initialized bot nickname. Nick: "+bot_informations.username);
 });
 
-// Command example, pretty easy. Each callback passes as parameter the context.
-// Context data includes message info, timestamp, etc; check the official documentation or print ctx.
-bot.command('start', (ctx) => {ctx.reply('Bot started.')});
-
-// Hears, instead of command, check if the given word or regexp is CONTAINED in user input and not necessarly at beginning.
-bot.hears('ymca', (ctx) => ctx.reply("*sing* It's fun to stay at the Y.M.C.A.!"));
-bot.hears(/torino/i, (ctx) => ctx.reply("Someone said Torino!?"));
-
-// Inline query support (@yourbot query). Can be used anywhere, even in groups. It works just like @gif bot.
+// Usage: @yourbot channel. Retrieves: a list of last videos of 'channel' query input.
 bot.on('inline_query', ctx => {
-    let nickname = ctx.update.inline_query.query;  // If you analyze the context structure, query field contains our query.
-    if(nickname.length > 3){  // If user input is @yourbot /command
-        youtube.fetch_channel_uploads(nickname).then(data => {
-            console.log(data);
-            youtube.parse_list_videos(data).then(data2 => {
-                console.log(data2);
-                    
+    let nickname = ctx.update.inline_query.query;  // Take the nickname out of Telegraf context structure.
+    if(nickname.length > 3){  // If user input is longer than 3 characters
+        // Let's get last uploaded videos as data structure through YouTube API (thanks to our written model).
+        youtube.fetch_channel_uploads(nickname).then(structured_data => {
+            // Let's parse those structured data to get only essential informations for video listing.
+            youtube.parse_list_videos(structured_data).then(video_info => {
+                // Let's encapsulate those informations in a list that Telegraf API can digest.
                 let new_arr = [];
-                for(let k in data2){
+                for(let k in video_info){
                     new_arr[k] = {
                         type: 'video',
                         id: k,
-                        title: data2[k].title,
-                        description: data2[k].description,
-                        video_url: data2[k].url,
+                        title: video_info[k].title,
+                        description: video_info[k].description,
+                        video_url: video_info[k].url,
                         mime_type: "video/mp4",
-                        thumb_url: data2[k].thumb
+                        thumb_url: video_info[k].thumb
                     }
                 }
-                console.log("new_arr="+new_arr);
+                // Let's show this list to the user. Cache time is set to zero for development purposes.
                 return ctx.answerInlineQuery(new_arr, {cache_time: 0});
             });
         }).catch(error => {console.log("Promise error: "+error)});
     }
 })
 
-// Start bot polling in order to not terminate Node.js application.
 bot.startPolling();
