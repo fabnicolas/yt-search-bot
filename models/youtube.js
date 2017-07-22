27a67369f00202d1dc126312626a7b1e97012960
Finalgalaxy <finalgalaxy@gmail.com>
2017-07-22 18:38:34 +0200

    Added function to ease HTTP URL building with YouTube API key.

--- models/youtube.js
+++ models/youtube.js
@@ -4,16 +4,19 @@ const config = require('../config');
 function YouTube(){
     let api_base_url = "https://www.googleapis.com/youtube/v3/";
 
+    let yt_api_auth = function(url){
+        return api_base_url+url+"&key="+config.youtube_api_key;
+    }
+
     this.fetch_channel_uploads = function (youtube_nickname, number_results = 5) {
         return new Promise((resolve, reject) => {
-            request(api_base_url+"channels?part=contentDetails&forUsername="+youtube_nickname
-                    +"&key="+config.youtube_api_key, function(error, response, body) {
+            request(yt_api_auth("channels?part=contentDetails&forUsername="+youtube_nickname), function(error, response, body) {
                 if(error)  reject("ERROR: "+error);
                 else{
                     let parsed_body = JSON.parse(body);
                     if(parsed_body.pageInfo.totalResults > 0){
                         var upload_id = JSON.parse(body).items[0].contentDetails.relatedPlaylists.uploads;
-                        request(api_base_url+"playlistItems?part=snippet&playlistId="+upload_id+"&maxResults="+number_results+"&key="+config.youtube_api_key, function(error, response, body) {
+                        request(yt_api_auth("playlistItems?part=snippet&playlistId="+upload_id+"&maxResults="+number_results), function(error, response, body) {
                             if(error)   reject("ERROR: "+error);
                             else        resolve(JSON.parse(body));
                         });
